13c3e47ee143d9c6002185c08b12d29a24e1f28f
Chirag <chiraggarg1708@gmail.com>
2025-02-28 10:46:46 +0530

    [Chirag] ADD: Calculate the age of Harry

---
+++ AgeOfHarry.java
@@ -0,0 +1,13 @@
+// Create AgeOfHarry class to compute the current age of Harry
+class AgeOfHarry{
+	public static void main(String[] args){
+		// Create a variable birthYear to store the birth year of Harry
+		int birthYear = 2000;
+
+		// Calculate the current age of Harry and store it in variable currentAge
+		int currentAge = 2024 - 2000;
+
+		// Display the result with current age
+		System.out.print("Harry/s current age in 2024 is " + currentAge);
+	}
+}
